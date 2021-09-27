a8a392c60edf8fe945928abf18a69f7424d9a03d
nevzatpeker16 <77233554+nevzatpeker16@users.noreply.github.com>
2021-09-27 23:49:45 +0300

    Add project files.

---
+++ Controllers/TrainReservationController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Buisness.Abstract;
+using Entities.Concrete;
+
+namespace AdaYazilimCase.Controllers
+{
+    public class TrainReservationController : Controller
+    {
+         IReservationService _reservationService;
+        public TrainReservationController(IReservationService reservationService)
+        {
+            _reservationService = reservationService;
+        }
+
+
+
+
+        [HttpPost("reserveTrain")]
+       public IActionResult ReserveTrain(Tren tren,int rezervasyonYapilacakKisiSayisi, bool kisilerFarkliVagonlaraYerlestirilebilir)
+       {
+
+           var result = _reservationService.ReserveTrain(tren, rezervasyonYapilacakKisiSayisi,
+               kisilerFarkliVagonlaraYerlestirilebilir);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(result);
+            }
+        }
+    }
+}
